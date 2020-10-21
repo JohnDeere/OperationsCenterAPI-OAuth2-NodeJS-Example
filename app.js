f5854beec8ee438fadb61ae5bbd42f18df391af0
Sam Knepper <sambobkne@gmail.com>
2020-10-21 13:20:20 -0500

    replace jade for mustache templating

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mustacheExpress = require('mustache-express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,8 +11,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('mustache', mustacheExpress());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'mustache');
 
 app.use(logger('dev'));
 app.use(express.json());
